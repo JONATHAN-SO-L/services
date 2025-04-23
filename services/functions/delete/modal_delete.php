0a7037fe97bc54323d1713313e57d3758467787f
JONATHAN-SO-L <sanceznarval00@outlook.com>
2025-04-23 10:59:00 -0600

    Se genera cambio de nombre debido a error funtions -> functions y se agrega directorio delete donde se almacena la función de modal

---
+++ services/functions/delete/modal_delete.php
@@ -0,0 +1,23 @@
+<!-- Modal -->
+<div class="modal fade" id="Delete" role="dialog">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title"><strong>Eliminar registro #</strong></h4>
+            </div>
+
+            <div class="modal-body">
+            <p>¿Estás seguro de borrar el registro seleccionado?</p>
+            <p>Una vez eliminado no podrás recuperarlo y deberás ingresarlo nuevamente</p>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-sm btn-success" data-dismiss="modal">No, cancelar</button>
+                <a href="#" class="btn btn-sm btn-danger">Si, borrar</a>
+            </div>
+
+        </div>
+    </div>
+</div>
