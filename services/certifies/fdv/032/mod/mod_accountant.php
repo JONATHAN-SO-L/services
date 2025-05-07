d7653fbe16e7354948ba16f21ab6030ee417a87f
JONATHAN-SO-L <sanceznarval00@outlook.com>
2025-05-07 17:31:34 -0600

    Se agrega seleccionador de número de serie de contador

--- services/certifies/fdv/032/mod/mod_accountant.php
+++ services/certifies/fdv/032/mod/mod_accountant.php
@@ -32,7 +32,7 @@ section(); ?>
                         <form role="form" action="mod_val_accountant.php" method="POST" enctype="multipart/form-data">
                             <div>
                                 <label><i class="fa fa-tachometer"></i>&nbsp;Modelo del Contador de Partículas:</label>
-                                <select class="form-control" name="contador">
+                                <select class="form-control" name="contador" required>
                                 <option value=""> - Selecciona el modelo requerido - </option>
                                 <option value="Valor por defecto">Valor por defecto</option>
                                 </select>
@@ -47,6 +47,32 @@ section(); ?>
         </div>
     </div>
 </div>
+
+<!-- Vista al guardar el modelo -->
+
+<div class="container">
+            <div class="row">
+                <div class="col-sm-8">
+                    <div class="panel panel-warning">
+                        <div class="panel-heading text-center"><strong>Selecciona correctamente el número de serie del contador</strong></div>
+                            <div class="panel-body">
+                                <form role="form" action="mod_val_accountant.php" method="POST" enctype="multipart/form-data">
+                                    <div>
+                                        <label><i class="fa fa-barcode" aria-hidden="true"></i>&nbsp;Número de serie del Contador de Partículas:</label>
+                                        <select class="form-control" name="contador" required>
+                                            <option value=""> - Selecciona el número de serie requerido - </option>
+                                            <option value="Valor por defecto">132194</option>
+                                        </select>
+                                        <br>
+
+                                        <center><input class="btn btn-sm btn-danger" type="submit" value="Siguiente" name="guardar_contador"></center>
+                                    </div>
+                                </form>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 <?php end_section();
 }else{
