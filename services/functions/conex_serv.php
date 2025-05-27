aa11243112e47396bfed9ba814b525fd7dbb4914
JONATHAN-SO-L <sanceznarval00@outlook.com>
2025-05-27 11:05:13 -0600

    Se genera archivo de conexión con veco_servicios

---
+++ services/functions/conex_serv.php
@@ -0,0 +1,18 @@
+<?php
+
+$user="veco_dvi";
+$pwd="Vec83Dv19iSa@";
+$host="localhost";
+$db="veco_servicios";
+
+try {
+	$con = new PDO("mysql:host=$host;dbname=$db", $user, $pwd);      
+	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+catch(PDOException $e)
+{
+	echo "ERROR 100: Fail Connection - " . $e->getMessage();
+}
+
+?>
