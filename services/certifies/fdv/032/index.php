7beaba7ba24a0bd2fda7eb12dfbd01f80bf2a27d
JONATHAN-SO-L <sanceznarval00@outlook.com>
2025-06-03 17:50:31 -0600

    Se programa el muestreo de los últimos 30 registros y la búsqueda de los mismo, se programan el redireccionamiento del botón modificar

--- services/certifies/fdv/032/index.php
+++ services/certifies/fdv/032/index.php
@@ -5,11 +5,44 @@ if ($_SESSION['nombre'] != '' && $_SESSION['tipo'] == 'devecchi' || $_SESSION['t
   include '../../assets/layout.php';
   include '../../../functions/delete/modal_delete.php';
   section();
+
+  function mensaje_error() {
+        echo '
+            <div class="alert alert-danger alert-dismissible fade in col-sm-3 animated bounceInDown" role="alert" style="position:fixed; top:70px; right:10px; z-index:10;"> 
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+            <h4 class="text-center"><strong>OCURRIÓ UN ERROR</strong></h4>
+            <p class="text-center">
+            No se logró recibir información de parte del sistema, por favor, inténtalo de nuevo o contácta al Soporte Técnico.
+            </p>
+            </div>
+            ';
+    }
+
+  function mensaje_busqueda() {
+    echo '
+            <div class="alert alert-success alert-dismissible fade in col-sm-3 animated bounceInDown" role="alert" style="position:fixed; top:70px; right:10px; z-index:10;"> 
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+            <h4 class="text-center"><strong>Búsqueda exitosa</strong></h4>
+            <p class="text-center">
+            Por favor visualiza la información encontrada.
+            </p>
+            </div>
+            ';
+  }
 ?>
 
     <table>
     <a href="empresa.php"><button type="submit" value="Nuevo Certificado" name="" class="btn btn-success" style="text-align:center"><i class="fa fa-plus"></i>&nbsp;&nbsp;Nuevo Certificado</button></a>
-    <a href="../../../formats/fdv032/FDV-S-032_SGC.php" target="_blank"><button type="submit" value="Plantilla SGC" class="btn btn-danger" style="text-align:center"><i class="fa fa-file-pdf-o" aria-hidden="true"></i>&nbsp;&nbsp;Plantilla SGC</button></a>
+
+    <div class="btn-group">
+      <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown">
+      <i class="fa fa-file-pdf-o" aria-hidden="true"></i>&nbsp;&nbsp;Plantillas SGC <span class="caret"></span></button>
+      <ul class="dropdown-menu" role="menu">
+        <li><a href="../../../formats/fdv032/FDV-S-032_SGC.php" target="_blank">FDV-S-032 (Menor a 100 LPM)</a></li>
+        <li><a href="#">FDV-S-032 (100 LPM)</a></li>
+      </ul>
+    </div>
+
     <td>
     <tr>
     <button onClick="document.location.reload();" type="submit" class="btn btn-warning" data-toggle="tooltip" data-placement="top" title="Haz clic para actualizar los datos" HSPACE="10" VSPACE="10"><i class="fa fa-refresh fa-spin fa-fw"></i>
@@ -23,9 +56,9 @@ if ($_SESSION['nombre'] != '' && $_SESSION['tipo'] == 'devecchi' || $_SESSION['t
     </div><br>
 
     <div class="container col-sm-4">
-      <form action="">
+      <form action="" method="POST" enctype="multipart/form-data">
         <label>Buscar</label>
-        <input class="form-control" type="search" name="words" placeholder="Puedes buscar por: Técnico, Compañía, Modelo CI ó Contador de Partículas" alt="Puedes buscar por: Técnico, Compañía, Modelo CI ó Contador de Partículas">
+        <input class="form-control" type="search" name="words" placeholder="Puedes buscar por: Compañía, Modelo CI ó Número de Seie de Contador" alt="Puedes buscar por: Técnico, Compañía, Modelo CI ó Contador de Partículas">
         <input class="btn btn-sm btn-success form-control-inline" type="submit" name="buscar" value="Buscar">
       </form><br>
     </div>
@@ -37,48 +70,171 @@ if ($_SESSION['nombre'] != '' && $_SESSION['tipo'] == 'devecchi' || $_SESSION['t
           <!-----------------------------------------------------------------
           TABLA CON TODOS LOS CERTIFICADOS EXPEDIDOS | LÍMITE DE 30 REGISTROS
           ------------------------------------------------------------------>
-          <div class="table-responsive">
-            <table class="table table-hover table-striped table-bordered">
-              <thead>
-                <tr>
-                <th class="text-center">Acción</th>
-                <th class="text-center">Folio / ID del documento</th>
-                <th class="text-center">Fecha del documento</th>
-                <th class="text-center">Compañía</th>
-                <th class="text-center">Modelo CI</th>
-                <th class="text-center">Contador de Partículas - No. Serie</th>
-                <th class="text-center">Fecha de Calibración</th>
-                <th class="text-center">Técnico Certificado</th>
-                <th class="text-center">Fecha y Hora de Cierre</th>
-                </tr>
-              </thead>
-              <tbody>
-                <td class="text-center">
-                  <a href="#" class="btn btn-sm btn-primary" title="Ver PDF"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
-                  <a href="./mod/modificar.php" class="btn btn-sm btn-warning" title="Modificar"><i class="fa fa-pencil-square" aria-hidden="true"></i></a>
-                  <button href="" class="btn btn-sm btn-danger" title="Eliminar" data-toggle="modal" data-target="#Delete"><i class="fa fa-trash" aria-hidden="true"></i></button>
-                </td>
-                <td class="text-center">25-041701</td>
-                <td class="text-center">17ABR25</td>
-                <td class="text-center">Biosense Webster</td>
-                <td class="text-center">150t-01</td>
-                <td class="text-center">154501</td>
-                <td class="text-center">19JUN24</td>
-                <td class="text-center">Jonathan Sánchez</td>
-                <td class="text-center">2025-04-17 14:01:25</td>
+          <?php
+          // Conexión a la DDBB
+          require '../../../functions/conex_serv.php';
+          $certified = 'fdv_s_032';
+          $tecnico = $_SESSION['nombre_completo'];
 
-                <tr></tr>
-              </tbody>
-            </table>
-          </div>
-          <!----------------------------------------
-          NO EXISTEN DATOS EN EL SISTEMA
-          ----------------------------------------->
-          <br>
-          <center><h2>No se encontraron certificados en el sistema</h2></center>
+          if (isset($_POST['buscar'])) {
+            // Se realiza la búsqueda de información
+            $word = $_POST['words'];
+
+            $s_register = $con->prepare("SELECT * FROM $certified 
+                                        WHERE tecnico = :tecnico 
+                                        AND empresa LIKE '%$word%' OR modelo_contador LIKE '%$word%' OR numero_serie LIKE '%$word%'
+                                        ORDER BY id_documento DESC LIMIT 30");
+            $s_register->bindValue(':tecnico', $tecnico);
+            $s_register->setFetchMode(PDO::FETCH_OBJ);
+            $s_register->execute();
+            $f_register = $s_register->fetchAll();
+
+            // Se realiza conteo de resultados
+            $total_registers = $con->prepare("SELECT COUNT(*) FROM $certified
+                                              WHERE tecnico = '$tecnico'
+                                              AND empresa LIKE '%$word%' OR modelo_contador LIKE '%$word%' OR numero_serie LIKE '%$word%'");
+            $total_registers->execute();
+            $num_total_results = $total_registers->fetchColumn();
+
+            if ($s_register -> rowCount() > 0) {
+              echo
+              '<div class="table-responsive">
+
+              <a href="index.php"><button type="submit" value="Volver" class="btn btn-primary" style="text-align:center"><i class="fa fa-reply"></i>&nbsp;&nbsp;Volver</button></a>
+              <br><br>
+              <p><strong>Búsqueda realizada: </strong><span class="badge bg-success">'.$word.'</span></p>
+              <p><strong>Registros encontrados: </strong><span class="badge bg-success">'.$num_total_results.'</span></p>
+
+                <table class="table table-hover table-striped table-bordered">
+                  <thead>
+                    <tr>
+                    <th class="text-center">Acción</th>
+                    <th class="text-center">Folio / ID del documento</th>
+                    <th class="text-center">Fecha del documento</th>
+                    <th class="text-center">Compañía</th>
+                    <th class="text-center">Modelo CI</th>
+                    <th class="text-center">Contador de Partículas - No. Serie</th>
+                    <th class="text-center">Fecha de Calibración</th>
+                    <th class="text-center">Técnico Certificado</th>
+                    <th class="text-center">Fecha y Hora de Cierre</th>
+                    </tr>
+                  </thead>';
+
+                  foreach ($f_register as $registro) {
+                    $id_documento = $registro -> id_documento;
+                    $fecha_documento = $registro -> fecha_documento;
+                    $empresa = $registro -> empresa;
+                    $modelo_contador = $registro -> modelo_contador;
+                    $numero_serie = $registro -> numero_serie;
+                    $fecha_calibracion = $registro -> fecha_calibracion;
+                    $tecnico_certificado = $registro -> tecnico;
+                    $fecha_hora_cierre = $registro -> fecha_hora_cierre;
+
+                    echo '
+                      <tbody>
+                        <td class="text-center">
+                          <a href="#" class="btn btn-sm btn-primary" title="Ver PDF"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                          <a href="./mod/modificar.php?'.$id_documento.'" class="btn btn-sm btn-warning" title="Modificar"><i class="fa fa-pencil-square" aria-hidden="true"></i></a>
+                          <button href="" class="btn btn-sm btn-danger" title="Eliminar" data-toggle="modal" data-target="#Delete"><i class="fa fa-trash" aria-hidden="true"></i></button>
+                        </td>
+                        <td class="text-center">'.$id_documento.'</td>
+                        <td class="text-center">'.$fecha_documento.'</td>
+                        <td class="text-center">'.$empresa.'</td>
+                        <td class="text-center">'.$modelo_contador.'</td>
+                        <td class="text-center">'.$numero_serie.'</td>
+                        <td class="text-center">'.$fecha_calibracion.'</td>
+                        <td class="text-center">'.$tecnico_certificado.'</td>
+                        <td class="text-center">'.$fecha_hora_cierre.'</td>
+
+                        <tr></tr>
+                      </tbody>
+                    ';
+                    mensaje_busqueda();
+                  }
+
+                echo '</table>
+              </div>';
+            } else {
+              mensaje_error();
+              echo '<a href="index.php"><button type="submit" value="Volver" class="btn btn-primary" style="text-align:center"><i class="fa fa-reply"></i>&nbsp;&nbsp;Volver</button></a>';
+              echo '<center><h2>No se encontraron certificados en el sistema</h2></center><br>';
+            }
+          } else {
+            // Se visualizan todos los registros
+            $s_register = $con->prepare("SELECT * FROM $certified WHERE tecnico = :tecnico ORDER BY id_documento DESC LIMIT 30");
+            $s_register->bindValue(':tecnico', $tecnico);
+            $s_register->setFetchMode(PDO::FETCH_OBJ);
+            $s_register->execute();
+            $f_register = $s_register->fetchAll();
+
+            // Se realiza conteo de resultados
+            $total_registers = $con->prepare("SELECT COUNT(*) FROM $certified WHERE tecnico = '$tecnico'");
+            $total_registers->execute();
+            $num_total_results = $total_registers->fetchColumn();
+
+            if ($s_register -> rowCount() > 0) {
+              echo
+              '<div class="table-responsive">
+                <table class="table table-hover table-striped table-bordered">
+                <p><strong>Registros Totales: </strong><span class="badge bg-success">'.$num_total_results.'</span></p>
+                  <thead>
+                    <tr>
+                    <th class="text-center">Acción</th>
+                    <th class="text-center">Folio / ID del documento</th>
+                    <th class="text-center">Fecha del documento</th>
+                    <th class="text-center">Compañía</th>
+                    <th class="text-center">Modelo CI</th>
+                    <th class="text-center">Contador de Partículas - No. Serie</th>
+                    <th class="text-center">Fecha de Calibración</th>
+                    <th class="text-center">Técnico Certificado</th>
+                    <th class="text-center">Fecha y Hora de Cierre</th>
+                    </tr>
+                  </thead>';
+
+                  foreach ($f_register as $registro) {
+                    $id_documento = $registro -> id_documento;
+                    $fecha_documento = $registro -> fecha_documento;
+                    $empresa = $registro -> empresa;
+                    $modelo_contador = $registro -> modelo_contador;
+                    $numero_serie = $registro -> numero_serie;
+                    $fecha_calibracion = $registro -> fecha_calibracion;
+                    $tecnico_certificado = $registro -> tecnico;
+                    $fecha_hora_cierre = $registro -> fecha_hora_cierre;
+
+                    echo '
+                      <tbody>
+                        <td class="text-center">
+                          <a href="#" class="btn btn-sm btn-primary" title="Ver PDF"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+                          <a href="./mod/modificar.php?'.$id_documento.'" class="btn btn-sm btn-warning" title="Modificar"><i class="fa fa-pencil-square" aria-hidden="true"></i></a>
+                          <button href="" class="btn btn-sm btn-danger" title="Eliminar" data-toggle="modal" data-target="#Delete"><i class="fa fa-trash" aria-hidden="true"></i></button>
+                        </td>
+                        <td class="text-center">'.$id_documento.'</td>
+                        <td class="text-center">'.$fecha_documento.'</td>
+                        <td class="text-center">'.$empresa.'</td>
+                        <td class="text-center">'.$modelo_contador.'</td>
+                        <td class="text-center">'.$numero_serie.'</td>
+                        <td class="text-center">'.$fecha_calibracion.'</td>
+                        <td class="text-center">'.$tecnico_certificado.'</td>
+                        <td class="text-center">'.$fecha_hora_cierre.'</td>
+
+                        <tr></tr>
+                      </tbody>
+                    ';
+                  }
+
+                echo '</table>
+              </div>';
+            } else {
+              echo '<center><h2>No se encontraron certificados en el sistema</h2></center>';
+            }
+          }
+          ?>
 
         </div>
       </div>
+
+      <?php include '../../../assets/footer.php'; ?>
+
     </div>
 
 <?php
