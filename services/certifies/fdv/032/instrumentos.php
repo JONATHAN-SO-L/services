670899e6d3f6208e0145a4a3022c88a36387b308
JONATHAN-SO-L <sanceznarval00@outlook.com>
2025-06-03 17:52:02 -0600

    Se recopila la información de los instrumentos CALIBRADOS para seleccionarlos en los instrumentos utilizados para la calibración

--- services/certifies/fdv/032/instrumentos.php
+++ services/certifies/fdv/032/instrumentos.php
@@ -4,6 +4,23 @@ session_start();
 if ($_SESSION['nombre'] != '' && $_SESSION['tipo'] == 'devecchi' || $_SESSION['nombre'] != '' && $_SESSION['tipo'] == 'admin') {
     include '../../assets/layout.php';
     section();
+
+    $id_documento = $_SERVER['QUERY_STRING'];
+
+    function mensaje_error() {
+        echo '
+            <div class="alert alert-danger alert-dismissible fade in col-sm-3 animated bounceInDown" role="alert" style="position:fixed; top:70px; right:10px; z-index:10;"> 
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+            <h4 class="text-center"><strong>OCURRIÓ UN ERROR</strong></h4>
+            <p class="text-center">
+            No se logró recibir información de los contadores en sistema, por favor, inténtalo de nuevo o contácta al Soporte Técnico.
+            </p>
+            </div>
+            ';
+    }
+
+    require '../../../functions/conex_serv.php';
+    $instruments = 'instrumentos';  // Tabla de instrumentos
 ?>
 
 <table>
@@ -17,7 +34,7 @@ if ($_SESSION['nombre'] != '' && $_SESSION['tipo'] == 'devecchi' || $_SESSION['n
     <div class="row" style="width: 770px;">
         <div class="col-sm-12">
             <div class="page-header2">
-                <h1>Instrumentos | Certificado: #</h1>
+                <h1>Instrumentos | Certificado: <strong><u><?php echo $id_documento; ?></u></strong></h1>
                 <span class="label label-danger"></span> 		 
                 <p class="pull-right text-primary"></p>
             </div>
@@ -31,36 +48,106 @@ if ($_SESSION['nombre'] != '' && $_SESSION['tipo'] == 'devecchi' || $_SESSION['n
             <div class="panel panel-success">
                 <div class="panel-heading text-center"><strong>Selecciona correctamente los instrumentos a utilizar</strong></div>
                     <div class="panel-body">
-                        <form role="form" action="estandard_trazabilidad.php" method="POST" enctype="multipart/form-data">
+                        <?php echo '<form role="form" action="../../../functions/add/instruments.php?'.$id_documento.'" method="POST" enctype="multipart/form-data">'; ?>
                             <div>
-                                <label><i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp;DMM <i>(Modelo)</i>:</label>
-                                <select class="form-control" name="contador" required>
-                                <option value=""> - Selecciona el modelo requerido - </option>
-                                <option value="Valor por defecto">Valor por defecto</option>
+                                <label><i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp;DMM <i>(Identificación VECO)</i>:</label>
+                                <select class="form-control" name="dmm" required>
+                                <option value=""> - Selecciona el instrumento requerido - </option>
+                                <?php
+                                    $s_instrument = $con->prepare("SELECT activo FROM $instruments WHERE estado = 'Calibrado' AND tipo_instrumento = 'DMM'");
+                                    $s_instrument->setFetchMode(PDO::FETCH_OBJ);
+                                    $s_instrument->execute();
+                                    $f_instrument = $s_instrument->fetchAll();
+
+                                    if ($s_instrument -> rowCount() > 0) {
+                                        foreach ($f_instrument as $dmm) {
+                                        $activo = $dmm -> activo;
+                                        echo '<option value="'.$activo.'">'.$activo.'</option>';
+                                        }
+                                    } else {
+                                        mensaje_error();
+                                    }
+                                ?>
                                 </select><br>
 
-                                <label><i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp;PHA <i>(Modelo)</i>:</label>
-                                <select class="form-control" name="contador" required>
-                                <option value=""> - Selecciona el modelo requerido - </option>
-                                <option value="Valor por defecto">Valor por defecto</option>
+                                <label><i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp;PHA <i>(Identificación VECO)</i>:</label>
+                                <select class="form-control" name="pha" required>
+                                <option value=""> - Selecciona el instrumento requerido - </option>
+                                <?php
+                                    $s_instrument = $con->prepare("SELECT activo FROM $instruments WHERE estado = 'Calibrado' AND tipo_instrumento = 'PHA'");
+                                    $s_instrument->setFetchMode(PDO::FETCH_OBJ);
+                                    $s_instrument->execute();
+                                    $f_instrument = $s_instrument->fetchAll();
+
+                                    if ($s_instrument -> rowCount() > 0) {
+                                        foreach ($f_instrument as $pha) {
+                                        $activo = $pha -> activo;
+                                        echo '<option value="'.$activo.'">'.$activo.'</option>';
+                                        }
+                                    } else {
+                                        mensaje_error();
+                                    }
+                                ?>
                                 </select><br>
 
-                                <label><i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp;Medidor de flujo de masa <i>(Modelo)</i>:</label>
-                                <select class="form-control" name="contador" required>
-                                <option value=""> - Selecciona el modelo requerido - </option>
-                                <option value="Valor por defecto">Valor por defecto</option>
+                                <label><i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp;Medidor de flujo de masa <i>(Identificación VECO)</i>:</label>
+                                <select class="form-control" name="mfm" required>
+                                <option value=""> - Selecciona el instrumento requerido - </option>
+                                <?php
+                                    $s_instrument = $con->prepare("SELECT activo FROM $instruments WHERE estado = 'Calibrado' AND tipo_instrumento = 'MFM'");
+                                    $s_instrument->setFetchMode(PDO::FETCH_OBJ);
+                                    $s_instrument->execute();
+                                    $f_instrument = $s_instrument->fetchAll();
+
+                                    if ($s_instrument -> rowCount() > 0) {
+                                        foreach ($f_instrument as $mfm) {
+                                        $activo = $mfm -> activo;
+                                        echo '<option value="'.$activo.'">'.$activo.'</option>';
+                                        }
+                                    } else {
+                                        mensaje_error();
+                                    }
+                                ?>
                                 </select><br>
 
-                                <label><i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp;RH/TEMP SENSOR <i>(Modelo)</i>:</label>
-                                <select class="form-control" name="contador" required>
-                                <option value=""> - Selecciona el modelo requerido - </option>
-                                <option value="Valor por defecto">Valor por defecto</option>
+                                <label><i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp;RH/TEMP SENSOR <i>(Identificación VECO)</i>:</label>
+                                <select class="form-control" name="rh_temp" required>
+                                <option value=""> - Selecciona el instrumento requerido - </option>
+                                <?php
+                                    $s_instrument = $con->prepare("SELECT activo FROM $instruments WHERE estado = 'Calibrado' AND tipo_instrumento = 'RH/TEMP'");
+                                    $s_instrument->setFetchMode(PDO::FETCH_OBJ);
+                                    $s_instrument->execute();
+                                    $f_instrument = $s_instrument->fetchAll();
+
+                                    if ($s_instrument -> rowCount() > 0) {
+                                        foreach ($f_instrument as $rh_temp) {
+                                        $activo = $rh_temp -> activo;
+                                        echo '<option value="'.$activo.'">'.$activo.'</option>';
+                                        }
+                                    } else {
+                                        mensaje_error();
+                                    }
+                                ?>
                                 </select><br>
 
-                                <label><i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp;Balómetro <i>(Modelo)</i>:</label>
-                                <select class="form-control" name="contador" required>
-                                <option value=""> - Selecciona el modelo requerido - </option>
-                                <option value="Valor por defecto">Valor por defecto</option>
+                                <label><i class="fa fa-info-circle" aria-hidden="true"></i>&nbsp;Balómetro <i>(Identificación VECO)</i>:</label>
+                                <select class="form-control" name="balometro" required>
+                                <option value=""> - Selecciona el instrumento requerido - </option>
+                                <?php
+                                    $s_instrument = $con->prepare("SELECT activo FROM $instruments WHERE estado = 'Calibrado' AND tipo_instrumento = 'Balometro'");
+                                    $s_instrument->setFetchMode(PDO::FETCH_OBJ);
+                                    $s_instrument->execute();
+                                    $f_instrument = $s_instrument->fetchAll();
+
+                                    if ($s_instrument -> rowCount() > 0) {
+                                        foreach ($f_instrument as $balometro) {
+                                        $activo = $balometro -> activo;
+                                        echo '<option value="'.$activo.'">'.$activo.'</option>';
+                                        }
+                                    } else {
+                                        mensaje_error();
+                                    }
+                                ?>
                                 </select><br>
 
                                 <center><input class="btn btn-sm btn-success" type="submit" value="Siguiente" name="guardar_contador"></center>
