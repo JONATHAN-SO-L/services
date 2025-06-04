a0ff9044a8a0500f632651c7d1ae20e96d7d26e1
JONATHAN-SO-L <sanceznarval00@outlook.com>
2025-06-04 10:02:06 -0600

    Se genera carga en auditlog

--- services/functions/add/accountant.php
+++ services/functions/add/accountant.php
@@ -24,8 +24,27 @@ if ($_SESSION['nombre'] != '' && $_SESSION['tipo'] == 'devecchi' || $_SESSION['t
         $val_save_accountant = $save_accontant->execute([$modelo_ci, $numero_serie, $identificacion_cliente, $id_documento]);
 
         if ($val_save_accountant) {
-            echo '<script>alert("Registro exitoso, continúa con el llenado de información")</script>';
-            echo '<meta http-equiv="refresh" content="0; url=../../certifies/fdv/032/form.php?'.$id_documento.'">';
+            // Información para auditlog
+            include '../../assets/timezone.php';
+            $fecha_hora_carga = date("d/m/Y H:i:s");
+            $tecnico = $_SESSION['nombre_completo'];
+
+            // Registro en log
+            $log = 'auditlog';
+            $movimiento = utf8_decode('El usuario '.$tecnico.' registró el contador '.$modelo_ci.' con número de serie '.$numero_serie.' el '.$fecha_hora_carga.'');
+            $url = $_SERVER['PHP_SELF'].'?'.$id_documento;
+            $database = 'SIS';
+            $save_move = $con->prepare("INSERT INTO $log (movimiento, link, ddbb, usuario_movimiento, fecha_hora)
+                                                  VALUES (?, ?, ?, ?, ?)");
+            $val_save_move = $save_move->execute([$movimiento, $url, $database, $tecnico, $fecha_hora_carga]);
+
+            if ($val_save_move) {
+                echo '<script>alert("Registro exitoso, continúa con el llenado de información")</script>';
+                echo '<meta http-equiv="refresh" content="0; url=../../certifies/fdv/032/form.php?'.$id_documento.'">';
+            } else {
+                echo '<script>alert("Ocurrió un problema al intentar guardar la información, por favor, inténtalo de nuevo o contacta al Soporte Técnico")</script>';
+                echo '<meta http-equiv="refresh" content="0; url=../../certifies/fdv/032/contador.php?'.$id_documento.'">';
+            }
         } else {
             echo '<script>alert("Ocurrió un problema al intentar guardar la información, por favor, inténtalo de nuevo o contacta al Soporte Técnico")</script>';
             echo '<meta http-equiv="refresh" content="0; url=../../certifies/fdv/032/contador.php?'.$id_documento.'">';
