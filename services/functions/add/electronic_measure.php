240208b0715e92b75ff4c2f3a34493d41c434b63
JONATHAN-SO-L <sanceznarval00@outlook.com>
2025-07-07 10:24:52 -0600

    Se agrega pantalla de carga

--- services/functions/add/electronic_measure.php
+++ services/functions/add/electronic_measure.php
@@ -5,6 +5,59 @@ if ($_SESSION['nombre'] != '' && $_SESSION['tipo'] == 'devecchi' || $_SESSION['t
 
     $id_documento = $_SERVER['QUERY_STRING'];
 
+    include '../../assets/admin/links.php';
+
+    function mensaje_ayuda(){
+    echo '
+    <div class="alert alert-success alert-dismissible fade in col-sm-3 animated bounceInDown" role="alert" style="position:fixed; top:70px; right:10px; z-index:10;"> 
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+    <h4 class="text-center"><strong>REGISTRO EXITOSO</strong></h4>
+    <p class="text-center">
+    Se registraron correctamente las mediciones electrónicas en el sistema.
+    </p>
+    </div>
+    ';
+    }
+
+    function mensaje_error() {
+        echo '
+            <div class="alert alert-danger alert-dismissible fade in col-sm-3 animated bounceInDown" role="alert" style="position:fixed; top:70px; right:10px; z-index:10;"> 
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+            <h4 class="text-center"><strong>OCURRIÓ UN ERROR</strong></h4>
+            <p class="text-center">
+            <u>No se logró recibir información correctamente, por favor, inténtalo de nuevo o contácta al Soporte Técnico.
+            </p>
+            </div>
+            ';
+    }
+
+    function redirect_failed($id_documento) {
+        echo '
+            <div class="container" style="margin-left: 40%">
+                <img src="../../assets/img/loading_dvi.gif" height="40%" weight="40%">
+                <br>
+                <a href="../../certifies/fdv/032/mediciones_electronicas.php?'.$id_documento.'" class="btn btn-sm btn-danger" style="margin-left: 15%">Regresar</a>
+            </div>';
+    }
+
+    function redirect_success($id_documento) {
+        echo '
+            <div class="container" style="margin-left: 40%">
+                <img src="../../assets/img/loading_dvi.gif" height="40%" weight="40%">
+                <br>
+                <a href="../../certifies/fdv/032/comportamiento.php?'.$id_documento.'" class="btn btn-sm btn-success" style="margin-left: 15%">Continuar</a>
+            </div>';
+    }
+
+    function redirect_success2($id_documento) {
+        echo '
+            <div class="container" style="margin-left: 40%">
+                <img src="../../assets/img/loading_dvi.gif" height="40%" weight="40%">
+                <br>
+                <a href="../../certifies/fdv/032/comportamiento2.php?'.$id_documento.'" class="btn btn-sm btn-success" style="margin-left: 15%">Continuar</a>
+            </div>';
+    }
+
     if (isset($_POST['guardar_mediciones'])) {
         require '../conex_serv.php';
         $certified = 'fdv_s_032';
@@ -62,11 +115,11 @@ if ($_SESSION['nombre'] != '' && $_SESSION['tipo'] == 'devecchi' || $_SESSION['t
 
                     if ($val_save_move) {
                         require '../drop_con.php';
-                        echo '<script>alert("Registro exitoso, continúa con el llenado de información")</script>';
-                        echo '<meta http-equiv="refresh" content="0; url=../../certifies/fdv/032/comportamiento.php?'.$id_documento.'">';
+                        mensaje_ayuda();
+                        redirect_success($id_documento);
                     } else {
-                        echo '<script>alert("Ocurrió un problema al intentar guardar la información, por favor, inténtalo de nuevo o contacta al Soporte Técnico")</script>';
-                        echo '<meta http-equiv="refresh" content="0; url=../../certifies/fdv/032/contador.php?'.$id_documento.'">';
+                        mensaje_error();
+                        redirect_failed($id_documento);
                     }
                 break;
                 
@@ -87,27 +140,22 @@ if ($_SESSION['nombre'] != '' && $_SESSION['tipo'] == 'devecchi' || $_SESSION['t
 
                     if ($val_save_move) {
                         require '../drop_con.php';
-                        echo '<script>alert("Registro exitoso, continúa con el llenado de información")</script>';
-                        echo '<meta http-equiv="refresh" content="0; url=../../certifies/fdv/032/comportamiento2.php?'.$id_documento.'">';
+                        mensaje_ayuda();
+                        redirect_success2($id_documento);
                     } else {
-                        echo '<script>alert("Ocurrió un problema al intentar guardar la información, por favor, inténtalo de nuevo o contacta al Soporte Técnico")</script>';
-                        echo '<meta http-equiv="refresh" content="0; url=../../certifies/fdv/032/contador.php?'.$id_documento.'">';
+                        mensaje_error();
+                        redirect_failed($id_documento);
                     }
-                break;
-
-                default:
-                    echo '<script>alert("Ocurrió un problema al intentar guardar la información, por favor, inténtalo de nuevo o contacta al Soporte Técnico")</script>';
-                    echo '<meta http-equiv="refresh" content="0; url=../../certifies/fdv/032/mediciones_electronicas.php?'.$id_documento.'">';
                 break;
             }
         } else {
-            echo '<script>alert("Ocurrió un problema al intentar guardar la información, por favor, inténtalo de nuevo o contacta al Soporte Técnico")</script>';
-            echo '<meta http-equiv="refresh" content="0; url=../../certifies/fdv/032/mediciones_electronicas.php?'.$id_documento.'">';
+            mensaje_error();
+            redirect_failed($id_documento);
         }
 
     } else {
-        echo '<script>alert("No se detectó el iniciador de la petición, por favor, inténtalo de nuevo o contacta al Soporte Técnico")</script>';
-        echo '<meta http-equiv="refresh" content="0; url=../../certifies/fdv/032/mediciones_electronicas.php?'.$id_documento.'">';
+        mensaje_error();
+        redirect_failed($id_documento);
     }
 
 } else {
