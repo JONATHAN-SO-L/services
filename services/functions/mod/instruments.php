375c947990b857c2732ac34338646065c59c8744
JONATHAN-SO-L <sanceznarval00@outlook.com>
2025-07-07 10:48:29 -0600

    Se agrega pantalla de carga y se arregla error en carga de datos a DDBB

--- services/functions/mod/instruments.php
+++ services/functions/mod/instruments.php
@@ -5,12 +5,55 @@ if ($_SESSION['nombre'] != '' && $_SESSION['tipo'] == 'devecchi' || $_SESSION['t
 
     $id_documento = $_SERVER['QUERY_STRING'];
 
+    include '../../assets/admin/links.php';
+
+    function mensaje_ayuda(){
+    echo '
+    <div class="alert alert-success alert-dismissible fade in col-sm-3 animated bounceInDown" role="alert" style="position:fixed; top:70px; right:10px; z-index:10;"> 
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+    <h4 class="text-center"><strong>MODIFICACIÓN EXITOSA</strong></h4>
+    <p class="text-center">
+    Se modificaron correctamente los instrumentos en el sistema.
+    </p>
+    </div>
+    ';
+    }
+
+    function mensaje_error() {
+        echo '
+            <div class="alert alert-danger alert-dismissible fade in col-sm-3 animated bounceInDown" role="alert" style="position:fixed; top:70px; right:10px; z-index:10;"> 
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+            <h4 class="text-center"><strong>OCURRIÓ UN ERROR</strong></h4>
+            <p class="text-center">
+            <u>No se logró recibir información correctamente, por favor, inténtalo de nuevo o contácta al Soporte Técnico.
+            </p>
+            </div>
+            ';
+    }
+
+    function redirect_failed($id_documento) {
+        echo '
+            <div class="container" style="margin-left: 40%">
+                <img src="../../assets/img/loading_dvi.gif" height="40%" weight="40%">
+                <br>
+                <a href="../../certifies/fdv/032/mod/mod_instruments.php?'.$id_documento.'" class="btn btn-sm btn-danger" style="margin-left: 15%">Regresar</a>
+            </div>';
+    }
+
+    function redirect_success($id_documento) {
+        echo '
+            <div class="container" style="margin-left: 40%">
+                <img src="../../assets/img/loading_dvi.gif" height="40%" weight="40%">
+                <br>
+                <a href="../../certifies/fdv/032/mod/modificar.php?'.$id_documento.'" class="btn btn-sm btn-success" style="margin-left: 15%">Continuar</a>
+            </div>';
+    }
+
     if (isset($_POST['guardar_cambios_instrumentos'])) {
         require '../conex_serv.php';
         $certified = 'fdv_s_032';
 
         // Recepción de datos
-
         # Primera fila
         $dmm_activo = $_POST['dmm_activo'];
         $dmm_modelo = $_POST['dmm_modelo'];
@@ -51,6 +94,10 @@ if ($_SESSION['nombre'] != '' && $_SESSION['tipo'] == 'devecchi' || $_SESSION['t
         $balometro_fecha_cal = $_POST['balometro_fecha_cal'];
         $balometro_prox_cal = $_POST['balometro_prox_cal'];
 
+        require '../../assets/timezone.php';
+        $fecha_documento = date('d/m/Y');
+        $fecha_documento = strftime('%d%b%y');
+
         //Información para auditlog
         $tecnico_mod = $_SESSION['nombre_completo'];
         include '../../assets/timezone.php';
@@ -62,14 +109,14 @@ if ($_SESSION['nombre'] != '' && $_SESSION['tipo'] == 'devecchi' || $_SESSION['t
                                                         mfm_activo=  ?, mfm_modelo = ?, mfm_numero_serie = ?, mfm_numero_control = ?, mfm_fecha_calibracion = ?, mfm_proxima_calibracion = ?,
                                                         rh_activo = ?, rh_modelo = ?, rh_numero_serie = ?, rh_numero_control = ?, rh_fecha_calibracion = ?, rh_proxima_calibracion = ?,
                                                         balometro_activo = ?, balometro_modelo = ?, balometro_numero_serie = ?, balometro_numero_control = ?, balometro_fecha_calibracion =?, balometro_proxima_calibracion = ?,
-                                                        modifica_data = ?, fecha_hora_modificacion = ?
+                                                        modifica_data = ?, fecha_hora_modificacion = ?, fecha_documento = ?
                                             WHERE id_documento = ?");
 
         $val_save_instruments = $save_instruments->execute([$dmm_activo, $dmm_modelo, $dmm_no_serie, $dmm_control_no, $dmm_fecha_cal, $dmm_prox_cal,
                                                             $pha_activo, $pha_modelo, $pha_no_serie, $pha_control_no, $pha_fecha_cal, $pha_prox_cal,
                                                             $mfm_activo, $mfm_modelo, $mfm_no_serie, $mfm_control_no, $mfm_fecha_cal, $mfm_prox_cal,
                                                             $rh_activo, $rh_modelo, $rh_no_serie, $rh_control_no, $rh_fecha_cal, $rh_prox_cal,
-                                                            $balometro_activo, $balometro_modelo, $balometro_no_serie, $balometro_control_no, $balometro_fecha_cal, $balometro_prox_cal, $tecnico_mod, $fecha_hora_carga, $id_documento]);
+                                                            $balometro_activo, $balometro_modelo, $balometro_no_serie, $balometro_control_no, $balometro_fecha_cal, $balometro_prox_cal, $tecnico_mod, $fecha_hora_carga, $fecha_documento, $id_documento]);
 
         if ($val_save_instruments) {
             // Registro en log
@@ -83,19 +130,20 @@ if ($_SESSION['nombre'] != '' && $_SESSION['tipo'] == 'devecchi' || $_SESSION['t
 
             if ($val_save_move) {
                 require '../drop_con.php';
-                echo '<script>alert("Registro exitoso, continúa con el llenado de información")</script>';
-                echo '<meta http-equiv="refresh" content="0; url=../../certifies/fdv/032/mod/modificar.php?'.$id_documento.'">';
+                mensaje_ayuda();
+                redirect_success($id_documento);
             } else {
-                echo '<script>alert("Ocurrió un error al intentar guardar la información en el auditlog, por favor, inténtalo de nuevo o contacta al Soporte Técnico")</script>';echo '<meta http-equiv="refresh" content="0; url=../../certifies/fdv/032/mod/mod_instruments.php?'.$id_documento.'">';
+                mensaje_error();
+                redirect_failed($id_documento);
             }
         } else {
-            echo '<script>alert("Ocurrión un error al intentar guardar la información, por favor, inténtalo de nuevo o contacta al Soporte Técnico")</script>';
-            echo '<meta http-equiv="refresh" content="0; url=../../certifies/fdv/032/mod/mod_instruments.php?'.$id_documento.'">';
+            mensaje_error();
+            redirect_failed($id_documento);
         }
 
     } else {
-        echo '<script>alert("No se detectó el iniciador de la petición, por favor, inténtalo de nuevo o contacta al Soporte Técnico")</script>';
-        echo '<meta http-equiv="refresh" content="0; url=../../certifies/fdv/032/mod/mod_instruments.php?'.$id_documento.'">';
+        mensaje_error();
+        redirect_failed($id_documento);
     }
 
 } else {
