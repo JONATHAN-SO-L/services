cf33de21e742b3a3a2f69dc8851519a57190b3c5
JONATHAN-SO-L <sanceznarval00@outlook.com>
2025-07-22 18:03:05 -0600

    Se actualiza la versión del software

--- services/assets/footer.php
+++ services/assets/footer.php
@@ -1,6 +1,6 @@
 <footer style="background: #2e2b2a; margin-top: 0%;">
     <div class="container">    
         <h4 class="text-center" style="color: white;">Software desarrollado propiedad de DE VECCHI INGENIEROS S.A. de C.V.</h4>
-        <h6 class="text-center" style="color: white;">v.1.0.0</h6>
+        <h6 class="text-center" style="color: white;">v.1.1.0</h6>
     </div>
 </footer>
